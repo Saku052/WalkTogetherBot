5c674da223fdf2f73f03f93e8e627dfe9f98b3ca
Sora Harada <92529068+Saku052@users.noreply.github.com>
2025-09-17 12:28:27 +0900

    Add afternoon tweet schedule for楽天モバイル promotion

--- index.js
+++ index.js
@@ -24,6 +24,10 @@ cron.schedule('0 9 * * *', () => {
   postTweet('おはようございます！楽天モバイル社員紹介キャンペーン。今なら相当なポイントがもらえます！');
 });
 
+cron.schedule('0 13 * * *', () => {
+  postTweet('楽天モバイル2,980円で無制限！全国エリア対応で快適通信✨社員紹介キャンペーン実施中🎉\nhttps://refer.rakuten.co.jp/participation/auth?campaign=referralmobile_en&inv=HogkjQ1qqpbL-ecSVo3nRWYOgZCZSNch4RHSC8VgWv4%3D&scid=su_15404');
+});
+
 cron.schedule('0 18 * * *', () => {
   postTweet('今日も1日お疲れ様です！楽天モバイル社員紹介キャンペーン。今なら相当なポイントがもらえます！');
 });
