9dc5be395b81167774be08c7d6c46e18548aae80
Sora Harada <92529068+Saku052@users.noreply.github.com>
2025-09-19 21:28:39 +0900

    Add COMMON_SUFFIX to tweet messages for楽天モバイル promotion to standardize closing remarks

--- tweets.js
+++ tweets.js
@@ -1,18 +1,31 @@
+const COMMON_SUFFIX = '\n\n▼キャンペーンはこちら\n\
+  https://r10.to/h5O3vB';
+
 const tweetMessages = {
-  text1: '節約・投資促進サラリーマンの皆さん、通信費を見直しませんか！楽天モバイルなら月々の費用を大幅削減！浮いたお金は投資やNISAや自己投資を加速。賢い選択で、もっと自由に使える資金を増やしましょう！ #楽天モバイル #節約術 #資産運用 #サラリーマンの味方\n\
-  https://refer.rakuten.co.jp/participation/auth?campaign=referralmobile_en&inv=HogkjQ1qqpbL-ecSVo3nRWYOgZCZSNch4RHSC8VgWv4%3D&scid=su_15404',
+  text1: '節約・投資促進サラリーマンの皆さん、通信費を見直しませんか！楽天モバイルなら月々の費用を大幅削減！\
+  浮いたお金は投資やNISAや自己投資を加速。賢い選択で、もっと自由に使える資金を増やしましょう！ \
+  #楽天モバイル #節約術 #資産運用 #サラリーマンの味方'
+  + COMMON_SUFFIX,
   
-  text2: 'ポイント活用・実質的支出削減出張費もランチ代も賢くポイントで！楽天モバイルは、楽天ポイントがザクザク貯まるのが魅力。貯まったポイントは楽天市場でフル活用。日々の消費をお得に変えて、実質的な支出削減を実現しませんか？ #楽天ポイント #ポイ活\n\
-  https://refer.rakuten.co.jp/participation/auth?campaign=referralmobile_en&inv=HogkjQ1qqpbL-ecSVo3nRWYOgZCZSNch4RHSC8VgWv4%3D&scid=su_15404',
+  text2: 'ポイント活用・実質的支出削減出張費もランチ代も賢くポイントで！楽天モバイルは、楽天ポイントがザクザク貯まるのが魅力\
+  貯まったポイントは楽天市場でフル活用。日々の消費をお得に変えて、実質的な支出削減を実現しませんか？ \
+  #楽天ポイント #ポイ活'
+  + COMMON_SUFFIX,
   
-  text3: 'データ無制限・ストレスフリー仕事もプライベートもギガを気にせず快適に！楽天モバイルならデータ無制限で、通勤中の動画視聴も（最近は）ストレスフリー。月末の速度制限に悩まされることなく快適な通信環境を手に入れましょう！ #楽天モバイル #データ無制限\n\
-  https://refer.rakuten.co.jp/participation/auth?campaign=referralmobile_en&inv=HogkjQ1qqpbL-ecSVo3nRWYOgZCZSNch4RHSC8VgWv4%3D&scid=su_15404',
+  text3: 'データ無制限・ストレスフリー仕事もプライベートもギガを気にせず快適に！楽天モバイルならデータ無制限で、\
+  通勤中の動画視聴も（最近は）ストレスフリー。月末の速度制限に悩まされることなく快適な通信環境を手に入れましょう！ \
+  #楽天モバイル #データ無制限'
+  + COMMON_SUFFIX,
   
-  text4: 'スマートな一本化・効率化仕事道具はスマートに一本化！楽天モバイルを中心に、楽天経済圏をフル活用。日々の業務や生活をシンプルに。効率を重視するあなたに、楽天モバイルでスマートなビジネスライフを。 #楽天経済圏 #スマホ決済 #効率化\n\
-  https://refer.rakuten.co.jp/participation/auth?campaign=referralmobile_en&inv=HogkjQ1qqpbL-ecSVo3nRWYOgZCZSNch4RHSC8VgWv4%3D&scid=su_15404',
+  text4: 'スマートな一本化・効率化仕事道具はスマートに一本化！楽天モバイルを中心に、楽天経済圏をフル活用。\
+  日々の業務や生活をシンプルに。効率を重視するあなたに、楽天モバイルでスマートなビジネスライフを。 \
+  #楽天経済圏 #スマホ決済 #効率化'
+  + COMMON_SUFFIX,
   
-  text5: '通勤時間の有効活用・自己投資通勤時間を自己投資のチャンスに！！楽天モバイルの安定した通信で、オンライン学習や最新ニュース収集もサクサク。移動時間を無駄にせず、スキルアップや情報収集に活用しませんか？ #自己投資 #通勤時間 #スキルアップ\n\
-  https://refer.rakuten.co.jp/participation/auth?campaign=referralmobile_en&inv=HogkjQ1qqpbL-ecSVo3nRWYOgZCZSNch4RHSC8VgWv4%3D&scid=su_15404'
+  text5: '通勤時間の有効活用・自己投資通勤時間を自己投資のチャンスに！！楽天モバイルの安定した通信で、\
+  オンライン学習や最新ニュース収集もサクサク。移動時間を無駄にせず、スキルアップや情報収集に活用しませんか？ \
+  #自己投資 #通勤時間 #スキルアップ'
+  + COMMON_SUFFIX,
 };
 
 module.exports = tweetMessages;
