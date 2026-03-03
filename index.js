3f8d17a9b986957daafab0eb140e8570780909cc
Sora Harada <92529068+Saku052@users.noreply.github.com>
2026-03-03 23:08:05 +0900

    efaw

--- index.js
+++ index.js
@@ -71,8 +71,8 @@ cron.schedule('58 21 * * 1-5', () => {
   timezone: "Asia/Tokyo"
 });
 
-// 平日 22時55分 - 75%成功率
-cron.schedule('55 22 * * 1-5', () => {
+// 平日 23時13分 - 75%成功率
+cron.schedule('13 23 * * 1-5', () => {
   postAIGeneratedTweet();
 }, {
   timezone: "Asia/Tokyo"
